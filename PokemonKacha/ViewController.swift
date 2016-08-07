237a58f143aba8aa28aad29b7610d2e5cec12f64
ChenHsinHsuan <aircon.chen@gmail.com>
2016-08-07 23:15:48 +0800

    init project

--- PokemonKacha/ViewController.swift
+++ PokemonKacha/ViewController.swift
@@ -7,9 +7,57 @@
 //
 
 import UIKit
-
+import AVFoundation
 class ViewController: UIViewController {
 
+    @IBOutlet weak var cameraView: UIView!
+    
+    var captureSession = AVCaptureSession()
+    var sessionOutput = AVCaptureStillImageOutput()
+    var previewLayer = AVCaptureVideoPreviewLayer()
+    var exportImage:UIImage?
+    
+    override func viewWillAppear(animated: Bool) {
+        
+        let devices = AVCaptureDevice.devicesWithMediaType(AVMediaTypeVideo)
+        
+        for device in devices {
+            if device.position == AVCaptureDevicePosition.Front{
+                do {
+                    let input = try AVCaptureDeviceInput(device: device as! AVCaptureDevice)
+                    
+                    if captureSession.canAddInput(input) {
+                        captureSession.addInput(input)
+                        sessionOutput.outputSettings = [AVVideoCodecKey:AVVideoCodecJPEG]
+                        
+                        if captureSession.canAddOutput(sessionOutput) {
+                            captureSession.addOutput(sessionOutput)
+                            captureSession.startRunning()
+                            
+                            previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+                            previewLayer.videoGravity = AVLayerVideoGravityResizeAspectFill
+                            previewLayer.connection.videoOrientation = AVCaptureVideoOrientation.Portrait
+                            cameraView.layer.addSublayer(previewLayer)
+                            
+                            previewLayer.position = CGPoint(x: self.cameraView.frame.width/2, y: self.cameraView.frame.height/2)
+                            
+                            previewLayer.bounds = cameraView.frame
+                            
+                        }
+                    }
+                } catch {
+                    print("Error!")
+                }
+                
+                
+            }
+        }
+        
+        
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +68,32 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func takePhotoButtonPressed(sender: UIButton) {
+        
+        if let videoConnection = sessionOutput.connectionWithMediaType(AVMediaTypeVideo) {
+            
+            sessionOutput.captureStillImageAsynchronouslyFromConnection(videoConnection, completionHandler: {
+                buffer, error in
+                
+//                let imageData = AVCaptureStillImageOutput.jpegStillImageNSDataRepresentation(buffer)
+//                
+//                let image = UIImage(data:imageData)!
+                
+                UIGraphicsBeginImageContextWithOptions(self.cameraView.frame.size, true, 0.0)
+                self.cameraView.layer.renderInContext(UIGraphicsGetCurrentContext()!)
+                self.exportImage = UIGraphicsGetImageFromCurrentImageContext()!
+                UIGraphicsEndImageContext()
+                
+                
+                
+                UIImageWriteToSavedPhotosAlbum(self.exportImage!, nil, nil, nil)
+                
+            })
+        }
+        
+        
+        
+    }
 
 }
 
